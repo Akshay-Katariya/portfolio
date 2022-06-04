acd5a77df6db9fc76b3fec601978496149e0af55
Akshay Katariya <akatari1@ford.com>
2022-06-04 18:54:50 +0530

    about section with image implemented

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -2,9 +2,21 @@ import React from 'react'
 import './about.css'
 
 export const About = () => {
+	const picture = require('../../../assets/coding.png')
 	return (
 		<div className='about'>
-			<div>This is about</div>
+			<div className='about-top'>
+				<div className='about-info'>
+					Hello There 👋, I am
+					<br />
+					<span className='about-name'>Akshay Katriya.</span>
+					<br />I love experimenting with mobile & web technologies
+				</div>
+				<div className='about-photo'>
+					<img src={picture} alt='profile' className='profile-pic' />
+				</div>
+			</div>
+			<div className='about-bottom'>Contact will go here</div>
 		</div>
 	)
 }
