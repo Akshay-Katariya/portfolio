cf9e778cca46a1f5117320eed0c644a5c6db625e
Akshay Katariya <akatari1@ford.com>
2022-06-04 17:55:25 +0530

    added mobile side bar/header

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -1,8 +1,41 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import './mobile.css'
 
-export const Mobile = () => {
-	return <div>Mobile Header</div>
+export const Mobile = (props) => {
+	const { isOpen, setIsOpen } = props
+	const handleCloseClick = () => setIsOpen(!isOpen)
+
+	return (
+		<div className='mobile'>
+			<div className='close-icon' onClick={handleCloseClick}>
+				<i class='fi fi-rr-cross-circle'></i>
+			</div>
+			<div className='mobile-options'>
+				<div className='mobile-option'>
+					<a href='#project'>
+						<i class='fi fi-rr-edit-alt option-icon'></i>Projects
+					</a>
+				</div>
+				<div />
+				<div className='mobile-option'>
+					<a href='#skills'>
+						<i class='fi fi-rr-laptop option-icon'></i>Skills
+					</a>
+				</div>
+				<div className='mobile-option'>
+					<a href='#work'>
+						<i class='fi fi-rr-briefcase option-icon'></i>Work
+					</a>
+				</div>
+				<div className='mobile-option'>
+					<a href='#project'>
+						<i class='fi fi-rr-user option-icon'></i>Contact
+					</a>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 Mobile.prototype = {
--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -1,5 +1,30 @@
 import React from 'react'
+import './web.css'
 
 export const Web = () => {
-	return <div>Web Header</div>
+	return (
+		<div className='web'>
+			<div className='web-option'>
+				<a href='#project'>
+					<i class='fi fi-rr-edit-alt option-icon'></i>Projects
+				</a>
+			</div>
+			<div />
+			<div className='web-option'>
+				<a href='#skills'>
+					<i class='fi fi-rr-laptop option-icon'></i>Skills
+				</a>
+			</div>
+			<div className='web-option'>
+				<a href='#work'>
+					<i class='fi fi-rr-briefcase option-icon'></i>Work
+				</a>
+			</div>
+			<div className='web-option'>
+				<a href='#project'>
+					<i class='fi fi-rr-user option-icon'></i>Contact
+				</a>
+			</div>
+		</div>
+	)
 }
